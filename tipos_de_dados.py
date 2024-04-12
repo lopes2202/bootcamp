73dbdf01a0f58b0bf1ab15476cd3a07fa2ba2c54
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-04-11 21:25:15 -0300

    bootcamp basico

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+print(11 + 10 + 10000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python") 
+
+int()
+float()
+str()
+bool()
+
+print(str("gabriel") + str(1))
---
+++ variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = 'Gabriel'
+idade = 20
+
+nome, idade = 'Gustavo' , 19 
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+
+BRAZILIAN_STATES = ["SP", "RJ", "DF"]
+
+
+print(BRAZILIAN_STATES)
+
