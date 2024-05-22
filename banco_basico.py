df324c8f53d70b5737fcc983d9b6f96007e4a0f0
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-05-22 18:15:05 -0300

    terminando banco basico

--- banco_basico.py
+++ banco_basico.py
@@ -6,12 +6,51 @@ menu = """
 
 """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+class ContaBancaria:
+    LIMITE_SAQUES = 3
+    LIMITE_VALOR_SAQUES = 500
 
+    def __init__(self):
+        self.saldo = 0
+        self.numero_saques = 0
+        self.operacoes = []        
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.operacoes.append(
+            f'R$ +{valor:.2f}'
+        )
+
+
+    def saque(self, valor):
+
+        if self.numero_saques >= self.LIMITE_SAQUES:
+            print("Você não pode mais realizar saques hoje")
+            return
+        elif valor > self.saldo:
+            print("Você não tem saldo suficiente")
+            return
+        elif valor > self.LIMITE_VALOR_SAQUES:
+            print(f"O saque não pode ser maior do que o limite de saque de R$ {self.LIMITE_VALOR_SAQUES:.2f}")
+            return
+
+        self.saldo = self.saldo - valor
+        self.numero_saques += 1
+        self.operacoes.append(
+            f'R$ -{valor:.2f}'
+        )
+
+    def imprimir_extrato(self):
+        print("\n================ EXTRATO ================")
+
+        for operacao in self.operacoes:
+            print(operacao)
+        print(f"\nSaldo: R$ {self.saldo:.2f}")
+        print("==========================================")
+
+
+    
+conta = ContaBancaria()
 while True:
 
     opcao = input(menu)
@@ -21,8 +60,7 @@ while True:
         deposito = float(input("Informe o valor do deposito: "))
 
         if deposito > 0:
-            saldo += deposito
-            extrato += f"Depósito: R$ {deposito:.2f}\n"
+            conta.depositar(deposito)
         
         else:
             print("Operação invalida, tente novamente!")
@@ -30,15 +68,15 @@ while True:
     elif opcao == "s":
         print("Saque")
         saque = float(input("Informe o valor do saque: "))
+        
+        
         if saque > 0:
-            saldo = saldo - saque
-            print(saldo)
+            conta.saque(saque)
+        else:    
+            print("Operação invalida, tente novamente!")
 
-        elif LIMITE_SAQUES > 3:
-            print("Você já atingiu o limite de saques de hoje")    
     elif opcao == "e":
-        print("Extrato")
-
+        conta.imprimir_extrato()           
     elif opcao == "q":
         break
 
